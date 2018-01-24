08f08601b8067d3c6d115a57ab916f458ba7e156
Patricio <pat.venegas.l@gmail.com>
2018-01-24 20:42:37 -0300

    se modifican los ejercicios se sacan los comentarios

--- desafiometodos.rb
+++ desafiometodos.rb
@@ -1,97 +1,97 @@
 # ejercicio 1
-# def par(x)
-#    x % 2 == 0
+def par(x)
+   x % 2 == 0
     
-# end
+end
 
-# puts par(2)
-# puts par(3)
-# puts par(4)
-# puts par(5)
+puts par(2)
+puts par(3)
+puts par(4)
+puts par(5)
 
 
 # ejercicio 2
 
-# def random
-#   result = [true, false].sample
+def random
+  result = [true, false].sample
  
-# end  
+end  
 
-# resultado = random
+resultado = random
 
-# 	if resultado == true
-# 	  puts 'sí'
-# 	elsif resultado == false
-# 	  puts 'no'
-# 	else
-# 	  puts 'error'
-# 	end
+	if resultado == true
+	  puts 'sí'
+	elsif resultado == false
+	  puts 'no'
+	else
+	  puts 'error'
+	end
 
 # Ejercicio 3
 
-# def check5(num)
-#  if num > 5
-#  	puts 'true'
-#  else
-#  	puts 'false'
-#  end	
-# end
+def check5(num)
+ if num > 5
+ 	puts 'true'
+ else
+ 	puts 'false'
+ end	
+end
 
-# puts check5(5) # Debería ser false
-# puts check5(6) # Debería ser true
+puts check5(5) # Debería ser false
+puts check5(6) # Debería ser true
 
 
 # Ejercicio 4
 
-# def saludar(saludo)
-# 	if saludo == 'hola'
-# 	puts 'Hola Mundo'
-# 	end
-# end
-# puts saludar('hola')
+def saludar(saludo)
+	if saludo == 'hola'
+	puts 'Hola Mundo'
+	end
+end
+puts saludar('hola')
 
 
 #ejercicio 5
 
 #impre  pares
 
-# def imprepares(a,b)
-#   x = []
-#   x.push(a)
-#  	x.push(b)
-#   	x.sort!
-#   for i in x[0]..x[1]
-#     puts i if i.even?
-#   end
-# end
+def imprepares(a,b)
+  x = []
+  x.push(a)
+ 	x.push(b)
+  	x.sort!
+  for i in x[0]..x[1]
+    puts i if i.even?
+  end
+end
 
-# imprepares(10,15)
+imprepares(10,15)
 
 
 # Ejercicio 6
 
-# def draw_line(size)
-#   '*' * size
-# end
+def draw_line(size)
+  '*' * size
+end
 
-# def draw_lines(size)
-#   size.times { puts draw_line(size) }
-# end
+def draw_lines(size)
+  size.times { puts draw_line(size) }
+end
 
-# draw_lines(5)
+draw_lines(5)
 
 
 #Ejercicio 7
 
-# def incluye? (frase,letra)
-#   frase.include?(letra)
-# end
+def incluye? (frase,letra)
+  frase.include?(letra)
+end
 
 
-# cadena = 'Hola Mundo!'
-# caracter = 'o'
+cadena = 'Hola Mundo!'
+caracter = 'o'
 
-# puts incluye?(cadena,caracter)
+puts incluye?(cadena,caracter)
 
 
 #ejercicio 8
